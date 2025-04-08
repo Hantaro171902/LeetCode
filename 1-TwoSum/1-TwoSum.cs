8d984d04fc6b5d2a615f741f9447189081ce9d24
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-04-08 21:38:36 +0700

    Create file

---
+++ 1-TwoSum/1-TwoSum.cs
@@ -0,0 +1,13 @@
+// Last updated: 4/8/2025, 9:38:30 PM
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i=0; i < nums.Length; i++) {
+            for (int j=i+1; j < nums.Length; j++) {
+                if (nums[j] + nums[i] == target) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
