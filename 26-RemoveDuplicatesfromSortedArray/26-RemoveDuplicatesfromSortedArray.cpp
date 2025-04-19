af33e402c4372ef7a51608cb44eed462df145137
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-04-19 22:20:06 +0700

    File updated at 4/19/2025, 10:19:41 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,13 +1,23 @@
-// Last updated: 4/19/2025, 10:08:29 PM
+// Last updated: 4/19/2025, 10:19:41 PM
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-                
             
-        sort(nums.begin(), nums.end()); // Optional if input is not sorted
-        auto k = unique(nums.begin(), nums.end());
-        nums.erase(k, nums.end());
-        return nums.size(); // Now the size matches unique elements
+        if (nums.empty()) return 0;
+        
+        int k = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[k] = nums[i]; 	// overwrite the unique value in k
+                k++;
+                
+                // cout << k << " ";
+            } 
+    //		else {
+    //			cout << k << " ";
+    //		}
+        }
+        return k;
     }
 
 };
