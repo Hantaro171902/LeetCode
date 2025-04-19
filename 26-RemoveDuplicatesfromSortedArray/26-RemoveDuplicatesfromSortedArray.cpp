debd394520a36937b2b0efded5999bc788005c9f
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-04-19 23:28:56 +0700

    File updated at 4/19/2025, 11:28:32 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,20 +1,11 @@
-// Last updated: 4/19/2025, 11:01:30 PM
+// Last updated: 4/19/2025, 11:28:32 PM
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        if (nums.empty()) return 0;
-	
-        int k = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                nums[k++] = nums[i]; 	
-    //			cout << k << endl;
-            } 
-    //		else {
-    //			nums.erase(remove(nums.begin(), nums.end(), i), nums.end());
-    //			cout << i << endl;
-    //		}
-        }
-        return k;
+    int strStr(string haystack, string needle) {
+     	int check = haystack.find(needle);
+        
+        if (check != string::npos) return check;
+        
+        return check;
     }
 };
