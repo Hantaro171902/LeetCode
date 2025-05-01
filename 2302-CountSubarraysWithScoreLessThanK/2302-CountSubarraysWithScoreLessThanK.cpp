60651e4d4aa8a984fcf999830911e26c3823d2e9
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-01 21:10:48 +0700

    Create file

---
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/1/2025, 9:10:34 PM
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = nums.size();
+        long long count = 0, total = 0;
+        for (int i = 0, j = 0; j < n; j++) {
+            total += nums[j];
+            while (i <= j && total * (j - i + 1) >= k) {
+                total -= nums[i];
+                i++;
+            }
+            count += j - i + 1;
+        }
+        return count;
+    }
+};
