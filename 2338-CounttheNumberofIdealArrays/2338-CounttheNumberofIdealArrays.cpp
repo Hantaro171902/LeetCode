3d620b388537d86887a63412ef5dd8103d6bdfc9
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-01 16:22:22 +0700

    File updated at 5/1/2025, 4:22:08 PM

--- 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.cpp
+++ 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.cpp
@@ -1,16 +1,31 @@
-// Last updated: 5/1/2025, 4:20:18 PM
+// Last updated: 5/1/2025, 4:22:08 PM
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        	int k = unordered_set<int>(nums.begin(), nums.end()).size(), res = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            unordered_set<int> st;
-            for (int j = i; j < nums.size(); j++) {
-                st.insert(nums[j]);
-                if (st.size() == k)
-                    res++;
+         int n = nums.size();
+    
+    // Count the number of unique elements in the entire array
+    unordered_set<int> allElements(nums.begin(), nums.end());
+    int totalDistinct = allElements.size();
+    
+    int result = 0;
+    unordered_map<int, int> freq;
+    int left = 0;
+    
+    for (int right = 0; right < n; ++right) {
+        freq[nums[right]]++;
+        
+        // Shrink the window from the left while it remains "complete"
+        while (freq.size() == totalDistinct) {
+            result += (n - right); // All subarrays starting from `left` to `right...n-1` are complete
+            freq[nums[left]]--;
+            if (freq[nums[left]] == 0) {
+                freq.erase(nums[left]);
             }
+            left++;
         }
-        return res;
+    }
+    
+    return result;
     }
 };
