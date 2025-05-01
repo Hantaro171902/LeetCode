b7f5a6735e6be89da1b525b43e5759ea7a7ba135
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-01 16:20:32 +0700

    File updated at 5/1/2025, 4:20:18 PM

--- 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.cpp
+++ 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.cpp
@@ -1,33 +1,16 @@
-// Last updated: 5/1/2025, 3:28:58 PM
+// Last updated: 5/1/2025, 4:20:18 PM
 class Solution {
 public:
-    
-    int digitSum(int n) {
-        int sum = 0;
-        while (n) {
-            sum += n % 10;
-            n /= 10;
-        }
-        return sum;
-    }
-
-    int countLargestGroup(int n) {
-        unordered_map<int, int> groupCount;
-        int maxSize = 0;
-        int count = 0;
-        
-        for (int i=1; i<=n; i++) {
-            int sum = digitSum(i);
-            groupCount[sum]++;
-            maxSize = max(maxSize, groupCount[sum]);
-        }
-        
-        for (auto& entry : groupCount) {
-            if (entry.second == maxSize) {
-                count++;
+    int countCompleteSubarrays(vector<int>& nums) {
+        	int k = unordered_set<int>(nums.begin(), nums.end()).size(), res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            unordered_set<int> st;
+            for (int j = i; j < nums.size(); j++) {
+                st.insert(nums[j]);
+                if (st.size() == k)
+                    res++;
             }
         }
-        
-        return count;
+        return res;
     }
 };
