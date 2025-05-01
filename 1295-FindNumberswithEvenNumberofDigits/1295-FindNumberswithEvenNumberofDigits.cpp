1ab2efcb6e3edd86f97418cfe4f4a32b95b5851a
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-01 23:40:10 +0700

    File updated at 5/1/2025, 11:39:56 PM

--- 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.cpp
+++ 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.cpp
@@ -1,41 +1,10 @@
-// Last updated: 5/1/2025, 3:28:07 PM
+// Last updated: 5/1/2025, 11:39:56 PM
 class Solution {
 public:
-    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-        int left = 0, right = min(tasks.size(), workers.size());
-
-        sort(tasks.begin(), tasks.end());
-        sort(workers.begin(), workers.end());
-
-        while(left < right) {
-            int mid = (left + right + 1) / 2;
-            int usedPills = 0;
-            multiset<int> workersFree(workers.end() - mid, workers.end());
-
-            bool canAssign = true;
-            for(int i = mid - 1; i >= 0; --i) {
-                auto it = prev(workersFree.end());
-
-                if(*it < tasks[i]) {
-                    it = workersFree.lower_bound(tasks[i] - strength);
-                    if(it == workersFree.end()) {
-                        canAssign = false;
-                        break;
-                    }
-                    ++usedPills;
-                    if(usedPills > pills) {
-                        canAssign = false;
-                        break;
-                    }
-                }
-                workersFree.erase(it);
-            }
-
-            if(canAssign)
-                left = mid;
-            else
-                right = mid - 1;
-        }
-        return left;
+    int findNumbers(vector<int>& nums) {
+        int ans=0;
+        for(int x:nums)
+            ans+=int(floor(log10(x))+1)%2==0;
+        return ans;
     }
 };
