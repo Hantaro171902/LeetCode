b76f435bfcee92c3f37bd43473e60f09499b10e9
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-01 16:44:31 +0700

    File updated at 5/1/2025, 4:44:17 PM

--- 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.cpp
+++ 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.cpp
@@ -1,31 +1,52 @@
-// Last updated: 5/1/2025, 4:43:29 PM
+// Last updated: 5/1/2025, 4:44:17 PM
+const int MOD = 1e9 + 7, MAX_N = 1e4 + 10,
+          MAX_P = 15;  // There are up to 15 prime factors
+int c[MAX_N + MAX_P][MAX_P + 1];
+vector<int> ps[MAX_N];  // List of prime factor counts
+int sieve[MAX_N];       // Minimum prime factor
+
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& nums) {
-         int n = nums.size();
-    
-    // Count the number of unique elements in the entire array
-    unordered_set<int> allElements(nums.begin(), nums.end());
-    int totalDistinct = allElements.size();
-    
-    int result = 0;
-    unordered_map<int, int> freq;
-    int left = 0;
-    
-    for (int right = 0; right < n; ++right) {
-        freq[nums[right]]++;
-        
-        // Shrink the window from the left while it remains "complete"
-        while (freq.size() == totalDistinct) {
-            result += (n - right); // All subarrays starting from `left` to `right...n-1` are complete
-            freq[nums[left]]--;
-            if (freq[nums[left]] == 0) {
-                freq.erase(nums[left]);
+    Solution() {
+        if (c[0][0]) {
+            return;
+        }
+        for (int i = 2; i < MAX_N; i++) {
+            if (sieve[i] == 0) {
+                for (int j = i; j < MAX_N; j += i) {
+                    sieve[j] = i;
+                }
             }
-            left++;
+        }
+        for (int i = 2; i < MAX_N; i++) {
+            int x = i;
+            while (x > 1) {
+                int p = sieve[x];
+                int cnt = 0;
+                while (x % p == 0) {
+                    x /= p;
+                    cnt++;
+                }
+                ps[i].push_back(cnt);
+            }
+        }
+        c[0][0] = 1;
+        for (int i = 1; i < MAX_N + MAX_P; i++) {
+            c[i][0] = 1;
+            for (int j = 1; j <= min(i, MAX_P); j++) {
+                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
+            }
         }
     }
-    
-    return result;
+    int idealArrays(int n, int maxValue) {
+        long long ans = 0;
+        for (int x = 1; x <= maxValue; x++) {
+            long long mul = 1;
+            for (int p : ps[x]) {
+                mul = mul * c[n + p - 1][p] % MOD;
+            }
+            ans = (ans + mul) % MOD;
+        }
+        return ans;
     }
 };
