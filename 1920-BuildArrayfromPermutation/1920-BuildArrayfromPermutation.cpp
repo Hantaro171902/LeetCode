23319c65885caee38a6b370a0dc3188683a56b7e
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-06 22:13:30 +0700

    Create file

---
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.cpp
@@ -0,0 +1,11 @@
+// Last updated: 5/6/2025, 10:13:30 PM
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans(nums.size(), 0);
+        for (int i = 0; i < nums.size(); i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
