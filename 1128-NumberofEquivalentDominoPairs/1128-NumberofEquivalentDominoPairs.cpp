6ca0e9d18f9e49df5350cd2a0d35441165052853
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-05-24 13:42:22 +0700

    File updated at 5/24/2025, 1:42:19 PM

--- 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.cpp
+++ 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.cpp
@@ -1,15 +1,23 @@
-// Last updated: 5/5/2025, 10:24:12 AM
+// Last updated: 5/24/2025, 1:42:19 PM
 class Solution {
 public:
-     const int mod=1e9+7;
-    //a[n]=2*a[n-1]+a[n-3] for n>=3
-    int numTilings(int n) {
-        array<int,3> a={1, 1, 2};
-        if (n<3) return a[n];
-        for(int i=3; i<=n; i++){
-            long long x=(2LL*a[2]+a[0])% mod;
-            a={a[1], a[2], (int)x};
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int, int> count;
+        int res = 0;
+        
+        for (const auto& d : dominoes) {
+            // Normalize domino: min first, then max
+            int a = min(d[0], d[1]);
+            int b = max(d[0], d[1]);
+            int key = a * 10 + b;	// Use 2-digit key sinceq 1 <= a, b <= 9
+            
+            cout << a << b << key;
+            
+            res += count[key];	// Add of number of existing dominoes that match this one
+            count[key]++;
+            
         }
-        return a[2];
+        
+        return res;	
     }
 };
