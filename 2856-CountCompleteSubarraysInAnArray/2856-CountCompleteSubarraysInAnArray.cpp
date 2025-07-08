3b6ea57cb1530be231becf3f4e4a83a8fca3525f
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-07-08 23:57:52 +0700

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/8/2025, 11:57:52 PM
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+         int n = nums.size();
+    
+    // Count the number of unique elements in the entire array
+    unordered_set<int> allElements(nums.begin(), nums.end());
+    int totalDistinct = allElements.size();
+    
+    int result = 0;
+    unordered_map<int, int> freq;
+    int left = 0;
+    
+    for (int right = 0; right < n; ++right) {
+        freq[nums[right]]++;
+        
+        // Shrink the window from the left while it remains "complete"
+        while (freq.size() == totalDistinct) {
+            result += (n - right); // All subarrays starting from `left` to `right...n-1` are complete
+            freq[nums[left]]--;
+            if (freq[nums[left]] == 0) {
+                freq.erase(nums[left]);
+            }
+            left++;
+        }
+    }
+    
+    return result;
+    }
+};
