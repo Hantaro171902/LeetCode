45138b65c0d7d68ac54f440431f79b2d61eb9a2a
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-07-08 23:57:36 +0700

    Create 3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes

---
+++ 3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes/3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/8/2025, 11:57:36 PM
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxVal = *max_element(nums.begin(), nums.end());
+        long long result = 0;
+        int left = 0, freq = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == maxVal)
+                freq++;
+
+            while (freq >= k) {
+                result += (n - right); // all subarrays from [left...right] to [left...n-1]
+                if (nums[left] == maxVal)
+                    freq--;
+                left++;
+            }
+        }
+
+        return result;
+    }
+};
