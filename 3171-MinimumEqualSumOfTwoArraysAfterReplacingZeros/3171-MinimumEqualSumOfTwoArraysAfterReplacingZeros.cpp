9a634c25c2f2f7f7e4474a3fe036b3f942d45c32
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-07-08 23:57:43 +0700

    Create 3171-MinimumEqualSumOfTwoArraysAfterReplacingZeros

---
+++ 3171-MinimumEqualSumOfTwoArraysAfterReplacingZeros/3171-MinimumEqualSumOfTwoArraysAfterReplacingZeros.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/8/2025, 11:57:42 PM
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    long long z1 = count(nums1.begin(), nums1.end(), 0), 
+              z2 = count(nums2.begin(), nums2.end(), 0);
+    long long s1 = accumulate(nums1.begin(), nums1.end(), z1),
+              s2 = accumulate(nums2.begin(), nums2.end(), z2);
+
+    if ((s1 < s2 && !z1) || (s2 < s1 && !z2)) return -1;
+    return max(s1, s2);
+    }
+};
