cc17d989eed43680468c2008ff608988586bc7e5
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-07-08 23:57:37 +0700

    Create 3307-FindTheMaximumSumOfNodeValues

---
+++ 3307-FindTheMaximumSumOfNodeValues/3307-FindTheMaximumSumOfNodeValues.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/8/2025, 11:57:37 PM
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        vector<long long> dp = {0, LLONG_MIN};
+        for (int n : nums) {
+            vector<long long> cnt = {dp[0] + n, dp[1] + n};
+            cnt[1] = max(cnt[1], dp[0] + (n ^ k));
+            cnt[0] = max(cnt[0], dp[1] + (n ^ k));
+            dp = cnt;
+        }
+        return dp[0];
+    }
+};
