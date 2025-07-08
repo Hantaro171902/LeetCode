3ecea487cfa7f235faa32cbe4d39f69a71219a46
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-07-08 23:57:55 +0700

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,14 @@
+// Last updated: 7/8/2025, 11:57:55 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return () => "Hello World";
+    
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
