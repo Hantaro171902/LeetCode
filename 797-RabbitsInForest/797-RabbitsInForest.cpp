05362d58da18f990e289f22e355a833815038d9b
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-07-08 23:58:26 +0700

    Create 797-RabbitsInForest

---
+++ 797-RabbitsInForest/797-RabbitsInForest.cpp
@@ -0,0 +1,34 @@
+// Last updated: 7/8/2025, 11:58:25 PM
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+            
+        int total = 0;
+        
+        sort (answers.begin(), answers.end());
+        
+        for (int i=0; i < answers.size();) {
+            int x = answers[i];
+            int count = 1;	// count the number of  answer
+            
+            // count how many times this answer appears in a row
+        
+            int j = i + 1;
+            while (j < answers.size() && answers[j] == x) {
+                count++;
+                j++;
+            }
+            
+            int groupSize = x + 1;
+        
+            int groupCount = ceil((double)count / groupSize);
+    //		cout << groupCount;
+            total += groupCount * groupSize;
+    //        cout << groupSize;
+            i = j; // move to the next answer;
+
+        }
+        
+        return total;
+    }
+};
