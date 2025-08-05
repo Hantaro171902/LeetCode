ed303a8ebb2a6f51c2d895b3f3a997128e3d46bb
Chau Han <97072454+Hantaro171902@users.noreply.github.com>
2025-08-05 23:17:04 +0700

    Create 3790-FruitsIntoBasketsIi

---
+++ 3790-FruitsIntoBasketsIi/3790-FruitsIntoBasketsIi.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/5/2025, 11:16:59 PM
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        vector<bool> used(baskets.size(), false); // Track used baskets
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            bool placed = false;
+            for (int j = 0; j < baskets.size(); j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+
+    }
+};
